806258c60928c3881db97d36324ba8ffa8be837a
Andre Fonseca <andrefonseca.aff@gmail.com>
2026-03-18 11:04:08 +0000

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Job Application Tracker
+    <div className="flex min-h-screen flex-col bg-white">
+      <main className="flex-1">
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
+              A better way to track your job applications
+            </h1>
+            <p className="text-muted-foreground mb-10 text-xl">
+              Capture, organize and manage your job search in one place
+            </p>
+            <div className="flex flex-col items-center gap-4">
+              <button>Start for free</button>
+              <p>Free forever. No credit card required.</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
